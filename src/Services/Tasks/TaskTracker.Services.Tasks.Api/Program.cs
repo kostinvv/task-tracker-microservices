52a6655d7b7049dac1a41ee8bf99b231a4a6ea96
Vladislav Kostin <vladislavkostin.ds@yandex.ru>
2025-11-02 16:52:36 +0300

    Подключить и настроить минимальную конфигурацию Swagger
    - установлены пакеты Swashbuckle.AspNetCore.Swagger, для работы Swagger;
    - подключены соответствующие сервисы в Program.cs.

--- src/Services/Tasks/TaskTracker.Services.Tasks.Api/Program.cs
+++ src/Services/Tasks/TaskTracker.Services.Tasks.Api/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "TaskTracker Service API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
