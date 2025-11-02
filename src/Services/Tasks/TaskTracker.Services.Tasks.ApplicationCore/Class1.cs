fdcafde2df8f749ff19c6c361a7527f3db5b27ce
Vladislav Kostin <vladislavkostin.ds@yandex.ru>
2025-11-02 11:45:33 +0300

    Создать структуру проекта

---
+++ src/Services/Tasks/TaskTracker.Services.Tasks.ApplicationCore/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace TaskTracker.Services.Tasks.ApplicationCore;
+
+public class Class1
+{
+}
---
+++ src/Services/Tasks/TaskTracker.Services.Tasks.Infrastructure/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace TaskTracker.Services.Tasks.Infrastructure;
+
+public class Class1
+{
+}
