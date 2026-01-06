1a6753cb5febdee86f25bd2414232573022c6713
Vladislav Kostin <vladislavkostin.ds@yandex.ru>
2026-01-06 15:25:49 +0300

    Удалить ненужную константу

--- src/Services/Tasks/TaskTracker.Services.Tasks.Api/ApiExtensions/AuthExtension.cs
+++ src/Services/Tasks/TaskTracker.Services.Tasks.Api/ApiExtensions/AuthExtension.cs
@@ -8,8 +8,6 @@ namespace TaskTracker.Services.Tasks.Api.ApiExtensions;
 
 public static class AuthExtension
 {
-    private const string JwtSecretKeySectionName = "JWT_SECRET_KEY";
-    
     public static void AddAuthConfiguration(
         this IServiceCollection services,
         IConfiguration configuration)
