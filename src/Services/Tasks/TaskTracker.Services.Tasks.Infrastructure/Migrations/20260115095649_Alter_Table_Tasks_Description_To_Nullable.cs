315b8b5d764839828da91999a1ca3878d8c96f7e
Vladislav Kostin <vladislavkostin.ds@yandex.ru>
2026-01-15 21:12:13 +0300

    Добавить файлы миграции Alter_Table_Tasks_Description_To_Nullable

---
+++ src/Services/Tasks/TaskTracker.Services.Tasks.Infrastructure/Migrations/20260115095649_Alter_Table_Tasks_Description_To_Nullable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Services.Tasks.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alter_Table_Tasks_Description_To_Nullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "character varying(1024)",
+                maxLength: 1024,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1024)",
+                oldMaxLength: 1024);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "character varying(1024)",
+                maxLength: 1024,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(1024)",
+                oldMaxLength: 1024,
+                oldNullable: true);
+        }
+    }
+}
